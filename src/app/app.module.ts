7d29162181cc424472b4be1590545aafaedac521
Moisis Mounir <moisis.george96@gmail.com>
2024-04-26 05:49:17 +0300

    Made initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeNavBarComponent } from './Shared/components/home-nav-bar/home-nav-bar.component';
+import { UserHomeComponent } from './features/users/user-home/user-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNavBarComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
