337d1cb979233c8b46e0d948e42cea3e8f03b48f
Zaituny <mohamedinv.580@gmail.com>
2024-05-12 23:07:18 +0300

    added even more html for the resturant list

--- src/app/features/users/user-restaurant-list/user-restaurant-list.component.ts
+++ src/app/features/users/user-restaurant-list/user-restaurant-list.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './user-restaurant-list.component.css'
 })
 export class UserRestaurantListComponent {
-
+  items = [["title1", "description1", "free", "100"],
+  ["title2", "desc2", "10", "150"],
+  ["title3", "desc3", "20", "200"],
+  ["title4", "desc4", "30", "250"],]
 }
